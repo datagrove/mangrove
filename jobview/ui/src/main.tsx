0619442d51b5d0e23a1ae2e88daf71838147e606
Jim Hurd <jimh@datagrove.com>
2023-04-14 11:35:51 -0400

    a

--- jobview/ui/src/main.tsx
+++ jobview/ui/src/main.tsx
@@ -1,5 +1,5 @@
 import './index.css'
-import { JSXElement, Component, createSignal, For, onMount, Show, createResource } from 'solid-js'
+import { JSXElement, Component, createSignal, For, onMount, Show, createResource, Switch, Match } from 'solid-js'
 import { render } from 'solid-js/web'
 import { Route, Routes, Router, A, useNavigate, useParams, hashIntegration } from "@solidjs/router"
 import { BackNav, H2 } from './widget/nav'
@@ -25,6 +25,8 @@ interface Runnable {
 // each search entry covers a time range
 interface SearchEntry {
     name: string
+    summary: string
+    type: string
     start: number // unix time
     end: number
 }
@@ -39,6 +41,8 @@ interface Dash {
 }
 
 const mockWs = new MockWs((data: Rpc<any>) => {
+
+    
     switch(data.method) {
         case 'init':  // the job runner should define this api to get title, options, etc.
             return {title: 'Mock Job View'}
@@ -47,19 +51,30 @@ const mockWs = new MockWs((data: Rpc<any>) => {
         case 'container':
             return ['Production', 'Test']
         case 'dash':
+            const n = Date.now()
+            const h : SearchEntry[] = []
+            for (let i = 0; i < 100; i++) {
+                h.push({
+                    name: "process",
+                    start: n - i * 1000,
+                    end: n - i * 1000 + 1000,
+                    type: "job",
+                    summary: "ran all tasks"
+                })
+            }
             // we need to periodically refresh this
             const jobs : Dash = {
                 job: [
-                    { "name": "process", 
-                    "description": "Run all jobs"
+                    { 
+                        "name": "process", 
+                        "description": "Run all jobs"
                 }
                 ],
                 runnable: [
-                    { name: "process" }
+                    { name: "process" ,
+                        next: n + 24*60*60*1000}
                 ],
-                history: [
-                    {
-                ]
+                history: h
             }
             return jobs
         case 'search':
@@ -73,7 +88,13 @@ const mockWs = new MockWs((data: Rpc<any>) => {
 })
 const cn = new Cn(mockWs)
 
+function mdate (n: number) : string {
+    return  new Date(n).toLocaleDateString('en-us', {year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'})
+}
 
+const Button = (props: {children: JSXElement}) => {
+    return <button class='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>{props.children}</button>
+}
 // we should show primary a list of logs, and maybe a drop down with jobs to run
 // we should show a list of jobs that will be run on a timer
 // we should show a list of jobs that have been run
@@ -81,12 +102,37 @@ const DatabasePage : Component = () => {
     const params = useParams()
     const [jobs] = createResource(params['db'], cn.get<Dash>('dash'))
     return <Page title={params['db']} back={'/'} >
-        <select>
-            <For each={jobs()?.value?.runnable}>{(e,i)=>{
-                return <option value={e.name}>e.name</option>
+        <Switch>
+            <Match when={!jobs()}>
+                Loading
+            </Match>
+            <Match when={jobs()!.error}>
+                {jobs()!.error}
+            </Match>
+        <Match when={true} >
+        <H2>Jobs</H2>
+        <table class='table-auto'>
+            <thead><tr><th>Name</th><th>Next Run</th><th></th></tr></thead>
+            <For each={jobs()!.value!.runnable}>{(e,i) => {
+                return <tr>
+                    <td class='border px-8 py-4'>{e.name}</td>
+                    <td class='border px-8 py-4 w-64'>{e.next?mdate(e.next):""}</td>
+                    <td class='border px-8 py-4'><Button>Run</Button></td>
+                </tr>
             }}</For>
-            </select><button>Run</button>
+        </table>
 
+        <H2>Recent</H2>
+        <table class='table-auto'>
+            <thead><tr><th>Start</th><th>End</th><th>Name</th><th>Summary</th></tr></thead>
+        <For each={jobs()!.value!.history}>{(e) => {
+            return <tr>
+                <td class='border px-8 py-4'>{mdate(e.start)}</td>
+                <td class='border px-8 py-4'>{mdate(e.end)}</td>
+                <td class='border px-8 py-4'>{e.name}</td>
+                <td class='border px-8 py-4'>{e.summary}</td></tr>
+        }}</For>
+        </table></Match></Switch>
         </Page>
 }
 
--- jobview/ui/src/widget/list.tsx
+++ jobview/ui/src/widget/list.tsx
@@ -126,3 +126,4 @@ export function ListView<T>(props: ListProps<T>) {
     
 }
 
+
