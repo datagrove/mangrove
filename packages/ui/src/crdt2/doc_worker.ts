7a10c6118070d2d72a09ddd1908571b0e2ae14be
jim <jimh@datagrove.com>
2023-06-10 18:51:08 -0400

    a

--- packages/ui/src/crdt2/doc_worker.ts
+++ packages/ui/src/crdt2/doc_worker.ts
@@ -16,16 +16,21 @@ class RebaseMap {
     }
 }
 class Context {
-    PositionMap = new RebaseMap()
+    positionMap = new RebaseMap()
+    dirty = new Set<string>() // all the dgid's that have changed.
+}
+class Node {
+    children: Node[] = []
 }
 class Rope {
 
+
     // just adjusts the tag location 
-    insertText(text: string,pos: number, length: number) {
+    insertText(text: string, pos: number, length: number) {
         // tags are added in the sparse domain
     }
     // inserting a tag can cause other tags to be invalidated
-    insertTag(tag: object, pos: number, length: number) : number[] {
+    insertTag(tag: object, pos: number, length: number): number[] {
         // tags are added in the sparse domain
         return []
     }
@@ -44,7 +49,7 @@ class TagTree {
     insertTag(tag: object, pos: number, length: number) {
         // tags are added in the sparse domain
     }
- 
+
 }
 
 
@@ -69,11 +74,17 @@ type DocState = {
 // so the tabstate can alert the buffers.
 // there's no great way a shared work to manage workers of its own. we could try using a leader, for now just duplicate the work
 
+// each object that we start tracking needs a unique id shared among all the buffers.
+// we can send this in patches, starting with the first. when the editor introduces a new object we need to create a new id.
+
+
 // we need to track the state of the buffer so we can send it accurate patches.
 // each buffer will have a different selection and could have different versions of the document 
 // maybe this is just a set of dirty nodes that we clean when the patch is accepted? we can't expect that the node ids are even related. so maybe the idea  of a buffer is just whack.
 class Buffer {
     // note that the strings here are node ids that are only meaningful to this single buffer.
+    dirtyGid = new Set<string>() // all the dgid's that have changed since last reconcilliation.
+    mapGid = new Map<string, string>() // map gid to node id
     dirty = new Set<string>()
     old = new Map<string, object>() // editor's view of the document.
     current = new Map<string, object>() // editor's view of the document.
@@ -86,7 +97,7 @@ class Buffer {
         // try to update the 
         for (let o of op) {
             this.dirty.add(o.path)
-            switch(o.op) {
+            switch (o.op) {
                 case "add":
                     this.current.set(o.path, o.value)
                     break;
@@ -97,16 +108,32 @@ class Buffer {
                     this.current.set(o.path, o.value)
             }
         }
-
         return
     }
 
     // the editor calls this when it has no changes of its own, but it wants to accept server changes. it gets a signal each time there are server changes, then it needs to call this to accept them.
-    async reconcile(sel: EditorSelection) : Promise<[JsonPatch[], EditorSelection]> {
+    async getChanges(doc: Doc, sel: EditorSelection): Promise<[JsonPatch[], EditorSelection]> {
+        // does lexical let us create our own node id? do we need it?
+        // when one buffer adds a node, we can assign it a dgid and add it to all the buffers.
+        // here we are mostly interested in nodes that have changed in the document.
+        // in the normal case, there is nothing changed here at all? should we even signal then?
+
+        let changes: JsonPatch[] = []
+
+        for (let dgid of this.dirtyGid) {
+            let node = doc.getNode(dgid)
+            changes.push({
+                op: "replace",
+                path: this.mapGid.get(dgid)??"", // what happens when the node doesn't have an id? can we just make one?
+                value: node
+            })
+        }
+
         this.dirty.clear()
+        this.dirtyGid.clear()
         // should we patch old, or just copy current?
-        this.old = {...this.current}
-        return [[],sel]
+        this.old = { ...this.current }
+        return [[], sel]
     }
 }
 
@@ -114,8 +141,9 @@ class Buffer {
 // maybe should give the document a message port to the localstate?
 // potentially give it a message port to each buffer?
 export class Doc implements DocApi {
-    buffer = new Map<number,Buffer>()
+    buffer = new Map<number, Buffer>()
     sessionId: number = Math.random()
+    tabstate?: ListenerApi
     ls!: LocalStateApi
     key: string = ""
     proposal?: Op[]
@@ -125,24 +153,37 @@ export class Doc implements DocApi {
     rope = new Rope()
     version = 0
 
+    getNode(dgid: string) : Node {
+        return new Node()
+    }
 
-    update(buffer: number, op: JsonPatch[], sel: EditorSelection): Promise<void> {
-        return this.buffer.get(buffer)!.update(op, sel)
+    async update(buffer: number, op: JsonPatch[], sel: EditorSelection): Promise<void> {
+        let b = this.buffer.get(buffer)
+        if (!b) {
+            b = new Buffer()
+            this.buffer.set(buffer, b)
+        }
+        b.updateCurrent(op)
+        if (!this.proposal) {
+            this.sendProposal()
+        }
     }
 
     // the main rule for lexical is that element nodes can't go inside text or decorator nodes.
     // decorators can't have length, so there is no inside.
-    // style nodes can be split arbitrarily so that they fit inside the elements
+    // style nodes can be split arbitrarily so that they fit inside the elements so this is the main thing we need to adjust. we need to delete the old style nodes and create new ones.
 
     // these can't fail or be invalid, we can invalidate later if they conflict with a local tag (lww)
     // maybe two apis, one for success, one for failure?
     // each buffer has a different sessionId? is it practical to combine them?
     // maybe the buffer number is the sessionId?
-    async globalUpdate(op: Op[], sessionId: number,  version: number) {
-
+    // maybe if there is a proposal and its not accepted, we don't want to notify any
+    async globalUpdate(op: Op[], sessionId: number, version: number) {
+        // note all the nodes that are changed by this update
+        let dirty = new Set<string>()
         // update our model
         for (let o of op) {
-            switch(o.type) {
+            switch (o.type) {
                 case "insert":
                     break;
                 case "tag":
@@ -156,7 +197,13 @@ export class Doc implements DocApi {
 
         // every buffer that's not sessionId now needs to be notified of the change
         // they will ignore the change if they have outstanding changes that the version doesn't include
+        // we can send one message to the tabstate and it can signal each buffer.
+        // we can prepare each buffer by adding the changes
+        for (let b of this.buffer.values()) {
 
+        }
+        this.tabstate?.update(this.key, this.version, this.version)
+        this.sendProposal()
 
         // why not use length in bytes or ops as the version instead of tracking both?
         this.version = version
@@ -171,22 +218,22 @@ export class Doc implements DocApi {
             }
         }
     }
- 
+
     async sendProposal(rebase?: boolean) {
         this.proposal = this.nextProposal
         if (this.proposal.length > 0) {
             this.nextProposal = []
             // send the proposal to the server
-            this.ls.propose(this.key,this.proposal, this.version, this.sessionId)
+            this.ls.propose(this.key, this.proposal, this.version, this.sessionId)
         }
 
         // two kinds of dirty? not right with editor, not in a proposal?
-        this.dirty.clear()
+        
     }
     // turn the patches into ops, and then mergeup
     // this gets call on either localstate changes or editor changes
     // these don't fail, but we need to be able invalidate tags and then patch the document to reflect that.
-    
+
 
 
 }
