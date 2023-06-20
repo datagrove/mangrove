fee775fd17952327aab753a56921f166fd205397
Jim Hurd <jimh@datagrove.com>
2023-06-20 18:26:19 -0400

    a

--- packages/ui/src/db/rtc.ts
+++ packages/ui/src/db/rtc.ts
@@ -1,104 +1,74 @@
 interface PeerConnectionConfig {
-    iceServers: RTCIceServer[];
+  iceServers: RTCIceServer[];
+}
+
+interface SignalingMessage {
+  type: string;
+  data?: any;
+}
+
+class WebRTCApp {
+  socket: WebSocket;
+  pc: RTCPeerConnection;
+  channel?: RTCDataChannel;
+
+  constructor(signalingUrl: string, config: PeerConnectionConfig) {
+    this.socket = new WebSocket(signalingUrl);
+    this.pc = new RTCPeerConnection(config);
+
+    this.pc.onicecandidate = (candidate) => {
+      this.socket.send(JSON.stringify({ type: 'icecandidate', data: candidate }));
+    };
+
+    this.pc.ondatachannel = (channel) => {
+      this.channel = channel.channel;
+      this.channel.onmessage = (message) => {
+        console.log('Received message:', message.data);
+      }
+    };
+
+    this.socket.onmessage = async (message) => {
+      switch (message.type) {
+        case 'offer':
+          await this.pc.setRemoteDescription(message.data);
+          const answer = await this.pc.createAnswer();
+          await this.pc.setLocalDescription(answer);
+          this.socket.send(JSON.stringify({ type: 'answer', data: answer }));
+          break;
+        case 'answer':
+          await this.pc.setRemoteDescription(message.data);
+          break;
+        case 'icecandidate':
+          await this.pc.addIceCandidate(message.data);
+          break;
+      }
+    };
   }
-  
-  interface SignalingMessage {
-    type: string;
-    data?: any;
+
+  async start() {
+    this.channel = this.pc.createDataChannel('test');
+    this.channel.onmessage = (message) => {
+      for (let i=0; i<10; i++) {
+        this.channel?.send(`Hello, world! ${i}`);
+      }
+    }
+    const offer = await this.pc.createOffer();
+    await this.pc.setLocalDescription(offer);
+    this.socket.send(JSON.stringify({ type: 'offer', data: offer }));
   }
-  
-  class SignalingChannel {
-    private socket: WebSocket;
-  
-    constructor(url: string) {
-      this.socket = new WebSocket(url);
-    }
-  
-    send(message: SignalingMessage) {
-      this.socket.send(JSON.stringify(message));
-    }
-  
-    onmessage: (message: SignalingMessage) => void = () => {};
+  async listen() {
+
   }
-  
-  class PeerConnection {
-     pc: RTCPeerConnection;
-     channel?: RTCDataChannel;
-  
-    constructor(config: PeerConnectionConfig) {
-      this.pc = new RTCPeerConnection(config);
-    }
-  
-    createDataChannel(label: string) {
-      this.channel = this.pc.createDataChannel(label);
-      return this.channel;
-    }
-  
-    async createOffer(): Promise<RTCSessionDescriptionInit> {
-      const offer = await this.pc.createOffer();
-      await this.pc.setLocalDescription(offer);
-      return offer;
-    }
-  
-    async setRemoteDescription(description: RTCSessionDescriptionInit) {
-      await this.pc.setRemoteDescription(description);
-    }
-  
-    async addIceCandidate(candidate: RTCIceCandidateInit) {
-      await this.pc.addIceCandidate(candidate);
-    }
-  
-    onicecandidate: (candidate: RTCIceCandidateInit) => void = () => {};
-    ondatachannel: (channel: RTCDataChannel) => void = () => {};
-  }
-  
-  class WebRTCApp {
-    private signaling: SignalingChannel;
-    private pc: PeerConnection;
-  
-    constructor(signalingUrl: string, config: PeerConnectionConfig) {
-      this.signaling = new SignalingChannel(signalingUrl);
-      this.pc = new PeerConnection(config);
-  
-      this.pc.onicecandidate = (candidate) => {
-        this.signaling.send({ type: 'icecandidate', data: candidate });
-      };
-  
-      this.pc.ondatachannel = (channel) => {
-        console.log('Data channel created:', channel.label);
-      };
-  
-      this.signaling.onmessage = async (message) => {
-        switch (message.type) {
-          case 'offer':
-            await this.pc.setRemoteDescription(message.data);
-            const answer = await this.pc.pc.createAnswer();
-            await this.pc.pc.setLocalDescription(answer);
-            this.signaling.send({ type: 'answer', data: answer });
-            break;
-          case 'answer':
-            await this.pc.setRemoteDescription(message.data);
-            break;
-          case 'icecandidate':
-            await this.pc.addIceCandidate(message.data);
-            break;
-        }
-      };
-    }
-  
-    async start() {
-      const channel = this.pc.createDataChannel('test');
-      console.log('Data channel created:', channel.label);
-  
-      const offer = await this.pc.createOffer();
-      await this.pc.pc.setLocalDescription(offer);
-  
-      this.signaling.send({ type: 'offer', data: offer });
-    }
-  }
-  
-  const app = new WebRTCApp('ws://localhost:8080', {
-    iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
-  });
-  
-  app.start();
+}
+
+const app1 = new WebRTCApp('ws://localhost:8080', {
+  iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
+});
+
+app1.start();
+
+const app2 = new WebRTCApp('ws://localhost:8080', {
+  iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
+});
+
+app2.listen()
--- webrtc/main.go
+++ webrtc/main.go
@@ -2,43 +2,55 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
+	"sync"
 
 	"github.com/gorilla/websocket"
 	"github.com/rs/cors"
 )
 
 type Message struct {
-	Type string `json:"type"`
-	Data string `json:"data"`
+	Type string          `json:"type"`
+	Data json.RawMessage `json:"data"`
 }
 
 var upgrader = websocket.Upgrader{}
 
+var waiting []*websocket.Conn = make([]*websocket.Conn, 0)
+var sg sync.WaitGroup
+var mu sync.Mutex
+
 func main() {
 	// Create a new CORS handler
 	c := cors.AllowAll()
-
+	sg.Add(2)
 	// Create a new HTTP handler with the CORS middleware
 	mux := http.NewServeMux()
 	mux.HandleFunc("/ws", handleWebSocket)
 	handler := c.Handler(mux)
 
 	// Start the server
+	log.Printf("listening on :8080")
 	log.Fatal(http.ListenAndServe(":8080", handler))
 }
 
+// this needs to send it to the the other connection
 func handleWebSocket(w http.ResponseWriter, r *http.Request) {
 	// Upgrade the HTTP connection to a WebSocket connection
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
 		return
 	}
 	defer conn.Close()
-
+	mu.Lock()
+	waiting = append(waiting, conn)
+	x := len(waiting) - 1
+	mu.Unlock()
+	sg.Done()
+	sg.Wait()
 	for {
 		// Read incoming message from the client
 		_, message, err := conn.ReadMessage()
@@ -46,6 +58,7 @@ func handleWebSocket(w http.ResponseWriter, r *http.Request) {
 			log.Println(err)
 			break
 		}
+		log.Printf("received: %s,%d", message, x)
 
 		// Parse the message as a JSON object
 		var msg Message
@@ -54,23 +67,26 @@ func handleWebSocket(w http.ResponseWriter, r *http.Request) {
 			log.Println(err)
 			continue
 		}
+		cx := waiting[(x+1)%2]
+		log.Printf("sending: %s,%d", message, x)
+		cx.WriteMessage(websocket.TextMessage, message)
 
 		// Handle the message based on its type
-		switch msg.Type {
-		case "offer":
-			// Handle offer message
-			fmt.Println("Received offer:", msg.Data)
-			// ...
-		case "answer":
-			// Handle answer message
-			fmt.Println("Received answer:", msg.Data)
-			// ...
-		case "ice":
-			// Handle ICE candidate message
-			fmt.Println("Received ICE candidate:", msg.Data)
-			// ...
-		default:
-			log.Println("Unknown message type:", msg.Type)
-		}
+		// switch msg.Type {
+		// case "offer":
+		// 	// Handle offer message
+		// 	fmt.Println("Received offer:", msg.Data)
+		// 	// ...
+		// case "answer":
+		// 	// Handle answer message
+		// 	fmt.Println("Received answer:", msg.Data)
+		// 	// ...
+		// case "ice":
+		// 	// Handle ICE candidate message
+		// 	fmt.Println("Received ICE candidate:", msg.Data)
+		// 	// ...
+		// default:
+		// 	log.Println("Unknown message type:", msg.Type)
+		// }
 	}
 }
