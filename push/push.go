321c5c36557dc5b068f1a4e4f95cc7edf85ce484
Jim Hurd <jimh@datagrove.com>
2023-06-23 13:13:37 -0400

    a

--- push/push.go
+++ push/push.go
@@ -1,13 +1,8 @@
-package server
+package push
 
 import (
-	"context"
-	"errors"
-	"log"
-	"sync"
 	"time"
 
-	firebase "firebase.google.com/go"
 	"firebase.google.com/go/messaging"
 	"github.com/cornelk/hashmap"
 
@@ -20,43 +15,74 @@ type LogId int64
 type DeviceId int64
 type UserId int64
 
-type Settings struct {
+// The idea is that his is one per node
+// the hashmap is here for future use in limiting push notifications
+// but the shard can check it anyway. this online map gives a cluster wide look at who's online.
+type NotifyDb struct {
+	qu     *sql.DB
+	online *hashmap.Map[UserId, *User]
+
+	settings chan PushSettings
+	log      chan LogChange
+	join     chan Join
+	query    chan Query
+	push     chan bool // write to channel to kick off a batch push. the batch will then write into the channel itself
+}
+
+const (
+	NameAndMessage = iota
+	NameOnly
+)
+
+type DaySettings struct {
+	Start int64
+}
+type TimeSettings struct {
+	Day []DaySettings
+}
+type DeviceSettings struct {
+	DeviceId
+	LogSettings
+}
+type LogSettings struct {
+	LogId
+	TimeSettings
+	Mute     int64
+	MuteTags []string
+}
+
+type PushSettings struct {
 	UserId
-	LogId 
-	Mute []int64
-	Device []DeviceId
+	LogId        // 0 means default
+	UserSettings LogSettings
+	Device       []DeviceSettings
+	Log          []LogSettings
+	Mode         int // time to turn back on.
 }
 type LogChange struct {
 	LogId  int64
 	Length int64
 }
-type Subscribe struct {
-	UserId 
-	LogId  
-	Length int64  // -1 to unsubscribe
-}
-type Online struct {
+type Join struct {
 	UserId
-	DeviceId
-	
+	LogId
+	Leave bool
 }
 
-// how hard is this to restore after losing a node?
-type NotifyDb struct {
-	qu     *sql.DB
-	online *hashmap.Map[UserId, *User]
-
-	settings chan Settings
-	log      chan LogChange
+// query in background, query when coming online.
+// query is broadcast, all answer it.
+type Query struct {
+	UserId
+	DeviceId // need to send back to the proxy
 }
-func (n *NotifyDb) Run() {
-
-type User struct {
-	length map[LogId]int64
-	mute   map[LogId]bool
+type QueryResult struct {
+	LogId  []int64
+	Length []int64
 }
 
-func NewNotifier(path string) (*NotifyDb, error) {
+type SenderFn func(DeviceId, data []byte) error
+
+func NewNotifier(path string, fn SenderFn) (*NotifyDb, error) {
 	sql, e := sql.Open("sqlite3", path)
 	if e != nil {
 		return nil, e
@@ -67,141 +93,63 @@ func NewNotifier(path string) (*NotifyDb, error) {
 		online: hm,
 	}
 
+	sql.Exec("create table if not exists settings (user integer, log integer, mute integer, device integer)")
+	sql.Exec("create table if not exists log (log integer, length integer)")
+	sql.Exec("create table if not exists follow (user integer, log integer)")
+	// lock screen notifications is an option
+	sql.Exec("create table notify subject, body, image, device")
+
+	ticker := time.NewTicker(2500 * time.Millisecond)
+	done := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case <-done:
+				return
+			case _ = <-ticker.C:
+				// push message batches
+				sql.Exec("select")
+			}
+		}
+	}()
+
+	go func() {
+		for {
+			select {
+			case s := <-r.settings:
+				sql.Exec("insert or replace into settings (user, log, mute, device) values (?, ?, ?, ?)", s.UserId, s.LogId, s.Mute, s.Device)
+			case l := <-r.log:
+				// update log
+				sql.Exec("insert or replace into log (log, length) values (?, ?, ?)", l.LogId, l.Length)
+			case j := <-r.join:
+				if j.Leave {
+					// remove from online
+					sql.Exec("delete from follow where user=? and log?", j.UserId, j.LogId)
+				} else {
+					sql.Exec("insert or replace into follow (user, log) values (?, ?, ?)", j.UserId, j.LogId)
+				}
+			case q := <-r.query:
+				sql.Exec("select log, length from follow join log on follow.log=log.log where user=?", q.UserId)
+
+			default:
+
+				o := &messaging.Notification{
+					Title:    v.Subject,
+					Body:     v.Message,
+					ImageURL: v.ImageURL,
+				}
+				_ = o
+
+			}
+
+		}
+	}()
+
 	return r, nil
 }
 
-// typically you might publish to a topic, and retrieve the subscribers to that topic
-// but you also need wild cards and mute? data leaking here of course
-type Notify struct {
-	Sid      int64
-	Topic    string
-	Subject  string // a thing? maybe for email
-	Message  string
-	ImageURL string
-}
-type NotifySettings struct {
-}
-
-// how do we rate limit?
-func (s *NotifyDb) Can(sess *Session, sid int64, cap int) bool {
-	return true
-}
-
-var errNoPermission = errors.New("no permission")
-
-func (s *NotifyDb) SendPush(v *Notify, data []byte) {
-	o := &messaging.Notification{
-		Title:    v.Subject,
-		Body:     v.Message,
-		ImageURL: v.ImageURL,
-	}
-	_ = o
-}
-func (s *NotifyDb) Notify(sess *Session, v *Notify) error {
-	if !s.Can(sess, v.Sid, 1) {
-		return errNoPermission
-	}
-	a, e := s.qu.SelectPush(context.Background(), v.Sid)
-	if e != nil {
-		return e
-	}
-	for _, r := range a {
-		if len(r.Mute) > 0 {
-			continue
-		}
-		s.SendPush(v, r.Mute)
-	}
-	return nil
-}
-
-// FcmBuffer batches all incoming push messages and send them periodically.
-type FcmBuffer struct {
-	fcmClient        *messaging.Client
-	dispatchInterval time.Duration
-	batchCh          chan *messaging.Message
-	wg               sync.WaitGroup
-}
-
-func NewFcmBuffer() (*FcmBuffer, error) {
-	// There are different ways to add credentials on init.
-	// if we have a path to the JSON credentials file, we use the GOOGLE_APPLICATION_CREDENTIALS env var
-	//os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", c.Firebase.Credentials)
-	// or pass the file path directly
-	//opts := []option.ClientOption{option.WithCredentialsFile("creds.json")}
-
-	// if we have a raw JSON credentials value, we use the FIREBASE_CONFIG env var
-	//os.Setenv("FIREBASE_CONFIG", "{...}")
-
-	// or we can pass the raw JSON value directly as an option
-	//opts := []option.ClientOption{option.WithCredentialsJSON([]byte("{...}"))}
-
-	app, err := firebase.NewApp(context.Background(), nil)
-	if err != nil {
-		return nil, err
-	}
-
-	fcmClient, err := app.Messaging(context.TODO())
-	if err != nil {
-		return nil, err
-	}
-	return &FcmBuffer{
-		fcmClient:        fcmClient,
-		dispatchInterval: 5 * time.Second,
-		batchCh:          make(chan *messaging.Message, 1000),
-	}, nil
-}
-
-func (b *FcmBuffer) SendPush(msg *messaging.Message) {
-	b.batchCh <- msg
-}
-
-func (b *FcmBuffer) sender() {
-	defer b.wg.Done()
-
-	// set your interval
-	t := time.NewTicker(b.dispatchInterval)
-
-	// we can send up to 500 messages per call to Firebase
-	messages := make([]*messaging.Message, 0, 500)
-
-	defer func() {
-		t.Stop()
-
-		// send all buffered messages before quit
-		b.sendMessages(messages)
-
-		log.Println("batch sender finished")
-	}()
-
-	for {
-		select {
-		case m, ok := <-b.batchCh:
-			if !ok {
-				return
-			}
-
-			messages = append(messages, m)
-		case <-t.C:
-			b.sendMessages(messages)
-			messages = messages[:0]
-		}
-	}
-}
-
-func (b *FcmBuffer) Run() {
-	b.wg.Add(1)
-	go b.sender()
-}
-
-func (b *FcmBuffer) Stop() {
-	close(b.batchCh)
-	b.wg.Wait()
-}
-
-func (b *FcmBuffer) sendMessages(messages []*messaging.Message) {
-	if len(messages) == 0 {
-		return
-	}
-	batchResp, err := b.fcmClient.SendAll(context.TODO(), messages)
-	log.Printf("batch response: %+v, err: %s \n", batchResp, err)
+type User struct {
+	length map[LogId]int64
+	mute   map[LogId]bool
 }
