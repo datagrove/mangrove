a702f80630dd1b7430a26eb7f564a4677ce1e01b
jim <jimh@datagrove.com>
2023-07-05 13:14:07 -0400

    a

--- packages/SampleEdit/src/main.tsx
+++ packages/SampleEdit/src/main.tsx
@@ -5,8 +5,9 @@ import { Show, createSignal } from "solid-js"
 import { Route, Routes, useNavigate } from "@solidjs/router";
 import { Composer } from "../../ui-solid/src"
 import { tools } from "./tools";
-import { ChallengeNotify, LoginApi, Login, LoginInfo, LoginOrRegister, LoginProvider } from "../../login-solid/src";
+import { ChallengeNotify, LoginApi, Login, LoginInfo, LoginOrRegister, LoginProvider, loginApi } from "../../login-solid/src";
 import { LanguageProvider } from "../../i18n-solid/src";
+import { Peer, WsChannel } from "packages/abc/src";
 
 
 const [login, setLogin] = createSignal(false)
@@ -14,42 +15,14 @@ const [login, setLogin] = createSignal(false)
 export function CustomEditor() {
     return <div><Composer tools={tools()}/></div>
 }
-const loginApi : LoginApi = {
-    loginpassword: function (user: string, password: string): Promise<[ChallengeNotify, string]> {
-        throw new Error("Function not implemented.");
-    },
-    loginpassword2: function (secret: string): Promise<[LoginInfo, string]> {
-        throw new Error("Function not implemented.");
-    },
-    register: function (name: string): Promise<any> {
-        throw new Error("Function not implemented.");
-    },
-    registerb: function (cred: any): Promise<[string, string]> {
-        throw new Error("Function not implemented.");
-    },
-    addpasskey: function (): Promise<any> {
-        throw new Error("Function not implemented.");
-    },
-    addpasskey2: function (cred: any): Promise<[string, string]> {
-        throw new Error("Function not implemented.");
-    },
-    login2: function (cred: any): Promise<LoginInfo> {
-        throw new Error("Function not implemented.");
-    },
-    login: function (deviceId: string): Promise<any> {
-        throw new Error("Function not implemented.");
-    },
-    recover: function (email: string, phone: string): Promise<void> {
-        throw new Error("Function not implemented.");
-    },
-    recover2: function (otp: string): Promise<void> {
-        throw new Error("Function not implemented.");
-    }
-}
+
 
 export function App() {
+    let peer : Peer = new Peer(new WsChannel("ws://localhost:3000/wss"))
+    const api = loginApi(peer)
+    
     const Signup = () => {
-        return <LoginProvider api={loginApi} setLogin={setLogin}>
+        return <LoginProvider api={api} setLogin={setLogin}>
             <LoginOrRegister  />
         </LoginProvider>
     }
